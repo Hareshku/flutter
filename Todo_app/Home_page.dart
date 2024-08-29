c2abd6bc01115832e4fb5a9a853d9a543b5ada04
Haresh <109500895+Hareshku@users.noreply.github.com>
2024-08-29 21:47:32 +0500

    Update Home_page.dart

--- Todo_app/Home_page.dart
+++ Todo_app/Home_page.dart
@@ -1,1 +1,166 @@
 
+// import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:todo_app/secondscreen.dart';
+import 'package:todo_app/pages/Toggle_menu.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List todolist = [
+    ['Learn React', false],
+    ['Learn flutter', false],
+    ['Learn Java', false],
+    ['Learn javaScript', false],
+    ['Learn AI', false],
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+      return Scaffold(
+        drawer: const ToggleMenu(),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+          backgroundColor: Colors.white30,
+          appBar: AppBar(
+            // leading: const ToggleMenu(),
+            title: const Text("Todo App Bar"),
+            centerTitle: true,
+            actions: <Widget>[
+              IconButton(onPressed: () {}, icon: const Icon(Icons.add_alert)),
+              PopupMenuButton(
+                itemBuilder: (BuildContext context) {
+                  return [
+                    const PopupMenuItem(child: Text('A')),
+                    const PopupMenuItem(child: Text('B')),
+                    const PopupMenuItem(child: Text('C')),
+                    const PopupMenuItem(child: Text('D')),
+                  ];
+                },
+              ),
+            ],
+            backgroundColor: Colors.deepPurple,
+            foregroundColor: Colors.white,
+          ),
+
+          body: ListView.builder(
+            itemCount: todolist.length,
+            itemBuilder: (BuildContext context, index) {
+              return Padding(
+                padding: const EdgeInsets.only(
+                  top: 20,
+                  bottom: 0,
+                  right: 20,
+                  left: 20,
+                ),
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Colors.black12,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        todolist[index][0],
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.delete, color: Colors.white),
+                        onPressed: () {
+                          Navigator.of(context).pushNamed(
+                            // MaterialPageRoute(builder: (context)=> const SecondScreen(data: "data from home",),
+                            // ),
+                            "/second"
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+
+          ),
+
+          floatingActionButton: Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                    child: Padding(
+                        padding: const EdgeInsets.only(
+                          bottom: 0,
+                          left: 20,
+                          right: 20,
+                        ),
+                        // padding:  const EdgeInsets.symmetric(
+                        //   horizontal: 20,
+                        // ),
+
+
+                      child: ElevatedButton(
+                        onPressed: (){
+                          Navigator.of(context).pushNamed("/infoPage");
+                        },
+                        child: Text('Add new Items'),
+                      ),
+
+
+                        // child: TextField(
+                        //   textAlign: TextAlign.center,
+                        //   decoration: InputDecoration(
+                        //     hintText: 'Add new todo items',
+                        //     filled: true,
+                        //     fillColor: Colors.deepPurple.shade200,
+                        //     enabledBorder: OutlineInputBorder(
+                        //       borderSide: const BorderSide(
+                        //         color: Colors.deepPurple,
+                        //       ),
+                        //       borderRadius: BorderRadius.circular(15),
+                        //     ),
+                        //     focusedBorder: OutlineInputBorder(
+                        //       borderSide: const BorderSide(
+                        //         color: Colors.deepPurple,
+                        //       ),
+                        //       borderRadius: BorderRadius.circular(15),
+                        //     ),
+                        //   ),
+                        // )
+                    )
+                ),
+
+                //
+                // FloatingActionButton(
+                //
+                //   onPressed: () {
+                //     ScaffoldMessenger.of(context).showSnackBar(
+                //        SnackBar(
+                //         content: const Text("button clicked"),
+                //         duration:const Duration(seconds: 5),
+                //         action: SnackBarAction(
+                //           onPressed: () {},
+                //           label: "click" ,
+                //
+                //         ),
+                //       ),
+                //     );
+                //   },
+                //   child: Text("Add Item"),
+                // ),
+
+              ],
+            ),
+          )
+
+      );
+    // );
+  }
+}
