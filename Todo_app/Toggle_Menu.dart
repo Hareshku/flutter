37ddb3ec6983f30510ef13f2053417d3c2807bff
Haresh <109500895+Hareshku@users.noreply.github.com>
2024-08-29 21:45:22 +0500

    Update Toggle_Menu.dart

--- Todo_app/Toggle_Menu.dart
+++ Todo_app/Toggle_Menu.dart
@@ -1,1 +1,49 @@
 
+import 'package:flutter/material.dart';
+
+class ToggleMenu extends StatelessWidget {
+  const ToggleMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+           const UserAccountsDrawerHeader(accountName: Text("Haresh"), accountEmail: Text("@example.com"),
+          currentAccountPicture: CircleAvatar(
+            child: ClipOval(
+              child: Image(
+                image: AssetImage('images/user-profile2-modified.png'),
+              ),
+            ),
+            ),
+            ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title:Text("Home"),
+            onTap: () {
+              print("all set");
+            },
+
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title:Text("Setting"),
+            onTap: () {
+              print("all set");
+            },
+
+          ),
+          ListTile(
+            leading: Icon(Icons.contact_mail),
+            title:Text("Contact"),
+            onTap: () {
+              print("all set");
+            },
+
+          ),
+        ],
+      ),
+    );
+  }
+}
